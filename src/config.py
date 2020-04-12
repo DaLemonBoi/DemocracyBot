42d511e71dd8a21ba6c7e08f9fdc3355b15a4a04
vardy <jarred.vardy@gmail.com>
2020-04-12 18:46:32 +1000

    Add mariadb access functionality

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_DEFAULT_DATABASE = os.environ.get("DB_DEFAULT_DATABASE")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = int(os.environ.get("DB_PORT"))
